276db7d7628234e04bc7d7034dbe73a9737dc876
James <4589250+Naught0@users.noreply.github.com>
2022-01-18 17:26:59 -0500

    Remove illegal character pairings

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "lodash.permutations";
 import { permutations, chunk, zip, uniqBy } from "lodash";
 
+const ILLEGAL_PAIRS = "bq bz cf cj cv cx fq fv fx fz gq gv gx hx hz jb jd jf jg jh jl jm jp jq jr js jt jv jw jx jy jz kq kx kz mx mz pq pv px qb qc qd qf qg qh qj qk ql qm qn qp qq qv qw qx qy qz sx tq vb vf vh vj vk vm vp vq vw vx wq wv wx xd xj xk xr xz yq yy zf zr zx".split(" ");
 
 export const App = () => {
   const [resp, setResp] = useState([]);
@@ -14,6 +15,10 @@ export const App = () => {
 
     // Filter out incorrect inputs
     perms = perms.filter(perm => zip(perm, data).every(combo => !(combo[0] === combo[1].letter && combo[1].state === STATE.INCORRECT)));
+
+    // Filter out any words with illegal character pairs in them
+    perms = perms.filter(perm => !ILLEGAL_PAIRS.some(pair => perm.join('').toLowerCase().includes(pair)));
+
     setResp(uniqBy(perms, e => e.join('')));
   }
 
