f8eab613d8c589b7caf3095728f3c6c2ad084d06
James E <4589250+Naught0@users.noreply.github.com>
2026-05-21 14:25:39 -0400

    More matches

--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -3,7 +3,7 @@ import { findWords, type RowLetter } from "./util";
 self.onmessage = (
   e: MessageEvent<{ rows: RowLetter[][]; maxMatches?: number }>,
 ) => {
-  const { rows, maxMatches = 5 } = e.data;
+  const { rows, maxMatches = 10 } = e.data;
   const words = findWords(rows, maxMatches);
   self.postMessage({ words });
 };
